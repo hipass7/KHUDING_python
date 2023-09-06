f13159af8c65b7e139e43a7df9b6e62030480bf6
hipass7 <64785177+hipass7@users.noreply.github.com>
2023-09-06 23:00:15 +0900

    230906

--- 230906/BOJ_18111/hipass.py
+++ 230906/BOJ_18111/hipass.py
@@ -1,1 +1,38 @@
 # 이런식으로 본인닉네임.py 로 만들어서 만들죠! 폴더는 제가 문제 선정하면 바로 만들어두겠습니다.
+
+N, M, B = map(int, input().split())
+B_origin = B
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+res_min = 987654321
+idx = 0
+
+for block in range(257):
+    cnt = 0
+    escape = False
+    B = B_origin
+    for i in graph:
+        for j in i:
+            if j > block:
+                cnt += (j - block) * 2
+                B += (j - block)
+            else:
+                if B >= (block - j):
+                    cnt += (block - j)
+                    B -= (block - j)
+                else:
+                    escape = True
+                    break
+        if escape:
+            break
+    if escape:
+        continue
+    
+    if cnt <= res_min:
+        res_min = cnt
+        idx = block
+        continue
+
+print(res_min, idx)
