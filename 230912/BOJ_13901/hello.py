48e0bab7e4f211ca9e0197cd9768878d4a8e04de
Jungmin Kim <jmkimm00@icloud.com>
2023-09-12 19:20:06 +0900

    hello.py modified

--- 230912/BOJ_13901/hello.py
+++ 230912/BOJ_13901/hello.py
@@ -1,19 +1,33 @@
-def move(x, y, direction, R, C, matrix, c):
+def move(x, y, directions, R, C, matrix):
     direction_list = [1, 2, 3, 4]
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+    count = 0
+    start = 0
+    stop = 0
 
-    i = direction_list.index(direction)
-    nx, ny = x + dx[i], y + dy[i]
-
-    if 0 <= nx < R and 0 <= ny < C and matrix[nx][ny] == '*':
-        matrix[nx][ny] = c
-        return (nx, ny)
-
+    while stop < 4:
+        d = directions[start]
+        i = direction_list.index(d)
+        
+        while True:
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C and matrix[nx][ny] == '*':
+                count += 1
+                stop = 0
+                matrix[nx][ny] = count
+                x, y = nx, ny
+                print(matrix) # 움직일때마다
+            else:
+                print("부딪혔다")
+                stop += 1
+                break
+        start = (start + 1) % len(directions)  
     return (x, y)
 
-#입력받기
-R,C = map(int, input().split())
+
+# 입력받기
+R, C = map(int, input().split())
 matrix = [['*'] * C for _ in range(R)]
 k = int(input())
 obs = [[-1, -1] for _ in range(k)]
@@ -24,14 +38,11 @@ sr, sc = map(int, input().split())
 location = (sr, sc)
 directions = [int(x) for x in input().split()]
 
-#초기 행렬 완성
+# 초기 행렬 완성
 for o in obs:
     matrix[o[0]][o[1]] = 'x'
 matrix[sr][sc] = 0
 
-#입력받은 방향순으로 iteration
-for k in range(len(directions)):
-    location = move(location[0], location[1], directions[k], R, C, matrix, k + 1)
+location = move(location[0], location[1], directions, R, C, matrix)
 
-print(matrix)
 print(*location)
