9dc09c22f9dfc5ea492391f9e5a2aadb722bfb83
Hipass7 <qjsro8763@daum.net>
2023-09-26 18:44:40 +0900

    230925 hipass.py

--- 230925/BOJ_2644/hipass.py
+++ 230925/BOJ_2644/hipass.py
@@ -1,1 +1,42 @@
 # 촌수계산
+
+from collections import deque
+
+n = int(input())
+a, b = map(int, input().split())
+
+a -= 1
+b -= 1
+
+graph = [[0] * n for _ in range(n)]
+visited = [0] * n
+m = int(input())
+for _ in range(m):
+    x, y = map(int, input().split())
+    x -= 1
+    y -= 1
+    graph[x][y] = 1
+    graph[y][x] = 1
+
+queue = deque()
+queue.append(a)
+result = []
+cnt = 0
+visited[a] = 1
+ox = False
+while queue:
+    temp = queue.popleft()
+    result.append(temp)
+    if temp == b:
+        ox = True
+        break
+    for i in range(n):
+        if graph[temp][i] == 1 and visited[i] == 0:
+            queue.append(i)
+            visited[i] = visited[temp] + 1
+
+    
+if not ox:
+    print(-1)
+else:
+    print(visited[b] - 1)
