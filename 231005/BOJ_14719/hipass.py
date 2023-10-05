b84c13f2f1608798d0621c81613b1a530644c672
hipass7 <64785177+hipass7@users.noreply.github.com>
2023-10-06 00:12:16 +0900

    231005 hipass.py

--- 231005/BOJ_14719/hipass.py
+++ 231005/BOJ_14719/hipass.py
@@ -1,2 +1,23 @@
 # 빗물
 
+h, w = map(int, input().split())
+graph = list(map(int, input().split()))
+
+total = 0
+for i in range(h):
+    water = False
+    cnt = 0
+    for j in range(w):
+        if graph[j] > i:
+            if water == False:
+                water = True
+            else:
+                total += cnt
+                cnt = 0
+
+        if graph[j] <= i:
+            if water:
+                cnt += 1
+                continue
+
+print(total)
